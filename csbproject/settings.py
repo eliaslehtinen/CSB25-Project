f4181b8a4550bd4a2fa8d330cc55af81142a0221
Elias Lehtinen <elias.a.lehtinen@gmail.com>
2025-10-16 01:53:19 +0300

    Show creation datetime when viewing note

--- csbproject/settings.py
+++ csbproject/settings.py
@@ -129,9 +129,11 @@ LOGIN_REDIRECT_URL = "/notes/"
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
+
+
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Cairo'
 
 USE_I18N = True
 
